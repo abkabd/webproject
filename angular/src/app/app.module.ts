31f10b4e2d190a34eb3b9e0c012320622a5c357d
abkabd <abdullayev.aybek6728@gmail.com>
2019-03-18 19:39:34 +0600

    upd

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,17 @@ import { MainComponent } from './components/main/main.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { AboutComponent } from './components/about/about.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+
+const appRoutes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'signUp', component: SignUpComponent},
+  {path: 'signIn', component: SignInComponent},
+  {path: '**', component: NotFoundComponent},
+]
 
 @NgModule({
   declarations: [
@@ -14,11 +26,15 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     MainComponent,
     SignUpComponent,
     AboutComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
